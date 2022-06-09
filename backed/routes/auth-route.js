10b4779f900d163dbc23fa4b957bf52db83736a7
parth <parth.dudhatra@aspiresoftserv.in>
2022-06-09 19:04:50 +0530

    new

--- backed/routes/auth-route.js
+++ backed/routes/auth-route.js
@@ -1,77 +1,121 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const User = require('../models/user');
-const Employee = require('../models/employee')
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken')
-const checkAuth = require('../middeware/check-auth');
-const { json } = require('express/lib/response');
-const res = require('express/lib/response');
-
+const User = require("../models/user");
+const Employee = require("../models/employee");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const checkAuth = require("../middeware/check-auth");
+const { json } = require("express/lib/response");
+const res = require("express/lib/response");
 
 // signup
-router.post('/signup',(req, res) => {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err) {
-            return res.json({ success : false, message : "Massing issue"})
-        } else {
-            const user = new User({
-                fullName : req.body.fullName,
-                email : req.body.email,
-                mobile : req.body.mobile,
-                password : hash,
-               })
-               user.save().then(() => {
-                    res.json({ success : true, message : "account has bben cerated"})
-               }).catch((err) => {
-                   if(err.code === 11000){
-                       return res.json({ success: false, message : "Email already exited"})
-                   }
-               })
-        }
-    }) 
-})
+router.post("/signup", (req, res) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      return res.json({ success: false, message: "Massing issue" });
+    } else {
+      const user = new User({
+        fullName: req.body.fullName,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => {
+          res.json({ success: true, message: "account has bben cerated" });
+        })
+        .catch((err) => {
+          if (err.code === 11000) {
+            return res.json({
+              success: false,
+              message: "Email already exited",
+            });
+          }
+        });
+    }
+  });
+});
 
 // login
-router.post('/login',(req, res) => {
-    User.find({email : req.body.email})
-        .exec()
-        .then((result) => {
-            if(result.length < 1) {
-                return res.json({ success :false, message : "User Not Found"})
-            }
-            const user = result[0];
-            console.log("....", result)
-            console.log(",.,.,.,.,", req.body.password)
-            console.log(user._id)
-            bcrypt.compare(req.body.password, user.password, (ret , err) => {
-                if(ret){
-                    const payload = {
-                        userId : user._id
-                    }
-                    console.log("------",user._id)
-                    const token = jwt.sign(payload, 'parth')
-                    return res.json({ success : true, token : token ,message : "Login Successfully"})
-                }else {
-                    return res.json( {success : false, message : "Password do not match"})
-                }
-            })
-        }).catch(err => {
-            res.json({ success :false, message : "Auth Failed"})
-        })
+// router.post('/login',(req, res) => {
+//     User.find({email : req.body.email})
+//         .exec()
+//         .then((result) => {
+//             if(result.length < 1) {
+//                 return res.json({ success :false, message : "User Not Found"})
+//             }
+//             const user = result[0];
+//             console.log("....", result)
+//             console.log(",.,.,.,.,", req.body.password)
+//             console.log(user._id)
+//             bcrypt.compare(req.body.password, user.password, (ret , err) => {
+//                 if(ret){
+//                     const payload = {
+//                         userId : user._id
+//                     }
+//                     console.log("------",user._id)
+//                     const token = jwt.sign(payload, 'parth')
+//                     return res.json({ success : true, token : token ,message : "Login Successfully"})
+//                 }else {
+//                     return res.json( {success : false, message : "Password do not match"})
+//                 }
+//             })
+//         }).catch(err => {
+//             res.json({ success :false, message : "Auth Failed"})
+//         })
+// });
+
+router.post("/login", (req, res) => {
+  User.find({ email: req.body.email })
+
+    .exec()
+
+    .then((result) => {
+      if (result.length < 1) {
+        return res.json({ success: false, message: "User not found" });
+      }
+
+      const user = result[0];
+
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (result) {
+          const payload = {
+            usreId: user._id,
+          };
+
+          const token = jwt.sign(payload, "web");
+
+          return res.json({
+            success: true,
+            token: token,
+            message: "Login Successful",
+          });
+        } else {
+          return res.json({
+            success: false,
+            message: "Password do not macthed",
+          });
+        }
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, message: "Auth Failed" });
+    });
 });
 // profile
-router.get('/profile',checkAuth,(req, res) => {
-    const userId = req.userData.userId;
-    User.findById(userId)
-        .exec()
-        .then((result) => {
-            res.json({success : true, data : result})
-        }).catch(err => {
-            res.json({ success : false, message : "Server error"})
-        })
-})
+router.get("/profile", checkAuth, (req, res) => {
+  const userId = req.userData.userId;
+  User.findById(userId)
+    .exec()
+    .then((result) => {
+      res.json({ success: true, data: result });
+    })
+    .catch((err) => {
+      res.json({ success: false, message: "Server error" });
+    });
+});
 
 // router.post('/create', (req, res, next) => {
 //     const employee = new Employee({
@@ -101,70 +145,74 @@ router.get('/profile',checkAuth,(req, res) => {
 
 // Add Employee
 
-router.post('/add-employee', (req, res, next) => {
-    Employee.create(req.body,(err, data)=>{
-        if(err) {
-            return next(error)
-        }else {
-            res.json(data)
-        }
-    })
-})
+router.post("/add-employee", (req, res, next) => {
+  Employee.create(req.body, (err, data) => {
+    if (err) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
 // get all employee
 
-router.get('/', (req, res, next) => {
-    Employee.find((err, data) => {
-        if(err) {
-            return next(error)
-        }else{
-            res.json(data)
-        }
-    })
-})
+router.get("/", (req, res, next) => {
+  Employee.find((err, data) => {
+    if (err) {
+      return next(error);
+    } else {
+      res.json(data);
+    }
+  });
+});
 
 //same as this all
 // Get Book by Id
 
 router.get("/:id", (req, res, next) => {
-    Employee.findById(req.params.id).then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: "Post not found!" });
-      }
-    });
+  Employee.findById(req.params.id).then((post) => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found!" });
+    }
   });
+});
 
 // Update the employee
 
 router.put("/:id", (req, res, next) => {
-    Employee.findByIdAndUpdate(req.params.id, {
-        $set : req.body
-    },(error, data) => {
-        if(error) {
-            return next(error)
-            console.log(error)
-        }else{
-            res.json(data)
-            console.log("Employee updated successfully")
-        }
-    })
-  });
+  Employee.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (error, data) => {
+      if (error) {
+        return next(error);
+        console.log(error);
+      } else {
+        res.json(data);
+        console.log("Employee updated successfully");
+      }
+    }
+  );
+});
 
 // Detete the employee
 
-router.delete('/delete-employee/:id',(req, res, next) => {
-    Employee.findByIdAndRemove(req.params.id,(err , data) => {
-        if(err) {
-            return next (error)
-        }else{
-            res.status(200).json({
-                msg : data
-            })
-        }
-    })
-})
+router.delete("/delete-employee/:id", (req, res, next) => {
+  Employee.findByIdAndRemove(req.params.id, (err, data) => {
+    if (err) {
+      return next(error);
+    } else {
+      res.status(200).json({
+        msg: data,
+      });
+    }
+  });
+});
 
 // QTgHowBxcjmbSfzi
 module.exports = router;
